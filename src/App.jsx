b5a1d3bdd90756ac4898c3a854c8d45d567a7b6a
williapa <williams.4049@osu.edu>
2024-05-04 17:38:26 -0700

    CLIENT

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Splash from './splash/Splash';
-import GameLayout from './game/GameLayout';
-import { DisableClicksProvider } from './hooks/useDisableClicks';
-
-const setYScroll = (bool) => {
-  if (bool) window.scrollTo(0,0);
-  document.body.style.overflowY = bool ? 'auto': 'hidden';
-}
-
-const IdPage = () => {
-  const params = useParams();
-  const [playerId, setPlayerId] = useState('');
-
-  const handleResize = () => {
-    const sizes = [
-      [1700, 1260],
-      [1400, 780],
-      [1100, 736],
-      [868, 556],
-      [640, 720]
-    ];
-    let sizeIndex = 0;
-    const { innerHeight, innerWidth } = window;
-    while (innerWidth < sizes[sizeIndex][0] && sizeIndex < (sizes.length - 1)) {
-      sizeIndex++;
-    }
-    setYScroll(innerHeight < sizes[sizeIndex][1]);
-  };
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    // just in case
-    let existingId = localStorage.getItem('playerId');
-    if (!existingId) {
-      existingId = uuidv4();
-      localStorage.setItem('playerId', existingId);
-    }
-    setPlayerId(existingId);
-  }, []);
-
-  return (
-    <DisableClicksProvider>
-      <GameLayout player={playerId} id={params.id}/>
-    </DisableClicksProvider>
-  );
-};
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/:id" element={<IdPage />} />
+        <Route path="/*" element={<Splash />} />
       </Routes>
     </Router>
   );
--- src/util/rowColToLetterCol.js
+++
@@ -1,3 +0,0 @@
-export default function rowColToLetterCol(row, col) {
-  return `${"ABCDEFGHIJK".charAt(row)}${col}`;
-};
